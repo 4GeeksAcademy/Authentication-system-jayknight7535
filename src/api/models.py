49aabf5a2f934e2f1092fa685780f77241fddf17
jayknight7535 <thewitchjay@gmail.com>
2025-07-10 00:08:53 +0000

    added back end of project

--- src/api/models.py
+++ src/api/models.py
@@ -1,14 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import (
+    DeclarativeBase, Mapped,
+    mapped_column, relationship,
+)
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash,
+)
+class Base(DeclarativeBase):
+  pass
 
-db = SQLAlchemy()
+db = SQLAlchemy(model_class=Base)
 
-class User(db.Model):
+class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = generate_password_hash(password)
+
+    def check_password_hash(self, other):
+        return check_password_hash(self.password, other)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 
     def serialize(self):
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import (
+    create_access_token, get_jwt_identity,
+    get_current_user, jwt_required
+)
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,29 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/signup", meathods= ["POST"])
+def signup():
+    User=User(**request.json)
+    db.session.add(User)
+    db.session.commit()
+    return "", 200
+
+@api.route("/login", meathods=["POST"])
+def login():
+    User= db.session.scalars(
+        db.select(User)
+        .filter_by(username=request.json["username"])
+    ).one_or_none()
+    if not User:
+        return jsonify(msg="not valid"), 401
+    if not User.check_password_hash(request.jason["password"]):
+        return jsonify(msg="not valid"), 401
+    return jsonify(token=create_access_token(User))
+
+@api.route("/secret", meathods=["GET"])
+@jwt_required()
+def the_secret ():
+    return jsonify(msg="it's not a very exciting secret"), 200
+
+
